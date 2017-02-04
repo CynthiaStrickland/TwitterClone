738ccbdc7e763281f215af025630073f1a184f15
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-03 16:16:28 -0800

    Initial Commit

--- HomeController.swift
+++ HomeController.swift
@@ -1,9 +1,19 @@
 //
-//  File.swift
+//  HomeController.swift
 //  TwitterClone
 //
 //  Created by Cynthia Whitlatch on 2/3/17.
 //  Copyright © 2017 Fenix Design. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HomeController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+        
+    
+    }
+}
